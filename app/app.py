db3c6942155b337976f1418b26cb5a2004b41422
tobyjordan <24980389+tobyjordan@users.noreply.github.com>
2018-11-10 17:35:29 +0000

    Created flask 'app' file.

--- app/app.py
+++ app/app.py
@@ -1,1 +1,16 @@
 import flask
+import datetime
+import pytz
+import aggregator
+
+app = flask.Flask("__name__")
+
+entries = aggregator.NewsAggregator().get_latest()
+
+latest_feed = entries[:5]
+curated_feed = entries[5:9]
+more_feed = entries[10:15]
+
+@app.route("/")
+def index():
+  return flask.render_template("index.html", latest_feed=latest_feed, curated_feed=curated_feed, more_feed=more_feed)
