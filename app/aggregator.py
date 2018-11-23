e55471c4f1a80c65ca28c26ee0e646a11bda5f93
tobyjordan <24980389+tobyjordan@users.noreply.github.com>
2018-11-23 19:35:56 +0000

    Re-formatted aggregator to use new feeds.

--- app/aggregator.py
+++ app/aggregator.py
@@ -6,6 +6,7 @@
 import feedparser
 from dateutil import parser
 import pytz
+import requests
 
 
 class NewsAggregator:
@@ -21,15 +22,16 @@ class NewsAggregator:
       "http://www.wsj.com/xml/rss/3_7085.xml",
       "https://www.aljazeera.com/xml/rss/all.xml"
     ]
+    self.cached_urls = {}
 
-  def get_latest(self, feeds=None):
+  def get_latest(self, feeds=None, count=None):
     """Return latest stories from a list of RSS feeds.
       Args:
         feeds (optional, array): An array of rss feed url's as strings.
     """
     entries = []
     for feed in (feeds if feeds else self._default_feeds):
-      _feed = NewsAggregator.feed_to_object(NewsAggregator.parse(feed))
+      _feed = self.feed_to_object(NewsAggregator.parse(feed))
       for entry in _feed["entries"]:
         _entry = entry
         _entry["source"] = _feed["name"]
@@ -37,7 +39,62 @@ class NewsAggregator:
         entries.append(_entry)
 
     entries.sort(key=lambda e: e["date"], reverse=True)
-    return entries
+    return entries if count is None else entries[:count]
+
+  def feed_to_object(self, parsed_feed):
+    """Given a feed, returned from feedparser, return a uniform object.
+      Args:
+        parsed_feed (feedparser.FeedParserDict): Parsed feed, returned from feedparser.
+    """
+    return {
+      "name": parsed_feed.feed.title,
+      "logo": self.get_feed_logo(parsed_feed.feed),
+      "subtitle": parsed_feed.feed.subtitle,
+      "entries": [
+        {
+          "title": entry.title.replace("&apos;", "'").replace("&#8217;", "'"),
+          "thumbnail": self.get_thumbnail(entry),
+          "link": entry.link,
+          "summary": NewsAggregator.format_summary(entry.summary),
+          "date": NewsAggregator.datetime_from_string(
+            entry.published if "published" in entry.keys() else entry.date if "date" in entry.keys() else "recent")
+        } for entry in (parsed_feed.entries[0:10] if len(parsed_feed.entries) >= 10 else parsed_feed.entries)
+      ]
+    }
+
+  def get_thumbnail(self, entry):
+    """Given an RSS entry, return a thumbnail if one exists.
+      Args:
+        entry (feedparser.FeedParserDict): Entry from which to extract a thumbnail URL.
+    """
+    try:
+      if "media_thumbnail" in entry.keys():
+        return entry["media_thumbnail"][0]["url"]
+      else:
+        return entry["media_content"][0]["url"]
+    except Exception:
+      return None
+
+  def get_feed_logo(self, feed):
+    """Given a string, return a python datetime object.
+      Args:
+        feed (feedparser.FeedParserDict): Feed from which to extract a logo URL.
+    """
+    try:
+      if "href" in feed["image"].keys():
+        _logo = feed["image"]["href"]
+      else:
+        _logo = feed["image"]["link"]
+    except Exception:
+      return None
+
+    if _logo not in self.cached_urls.keys():
+      try:
+        self.cached_urls[_logo] = requests.get(_logo).status_code in [200, 301, 302]
+      except Exception:
+        self.cached_urls[_logo] = None
+
+    return _logo if self.cached_urls[_logo] else None
 
   @staticmethod
   def parse(feed):
@@ -76,56 +133,6 @@ class NewsAggregator:
     _date = date.replace("EDT", "UTC-5").replace("EST", "UTC-5")
     _date = parser.parse(_date).astimezone(pytz.utc)
     return _date
-
-  @staticmethod
-  def get_thumbnail(entry):
-    """Given an RSS entry, return a thumbnail if one exists.
-      Args:
-        entry (feedparser.FeedParserDict): Entry from which to extract a thumbnail URL.
-    """
-    try:
-      if "media_thumbnail" in entry.keys():
-        return entry["media_thumbnail"][0]["url"]
-      else:
-        return entry["media_content"][0]["url"]
-    except Exception:
-      return None
-
-  @staticmethod
-  def get_feed_logo(feed):
-    """Given a string, return a python datetime object.
-      Args:
-        feed (feedparser.FeedParserDict): Feed from which to extract a logo URL.
-    """
-    try:
-      if "href" in feed["image"].keys():
-        return feed["image"]["href"]
-      else:
-        return feed["image"]["link"]
-    except Exception:
-      return None
-
-  @staticmethod
-  def feed_to_object(parsed_feed):
-    """Given a feed, returned from feedparser, return a uniform object.
-      Args:
-        parsed_feed (feedparser.FeedParserDict): Parsed feed, returned from feedparser.
-    """
-    return {
-      "name": parsed_feed.feed.title,
-      "logo": NewsAggregator.get_feed_logo(parsed_feed.feed),
-      "subtitle": parsed_feed.feed.subtitle,
-      "entries": [
-        {
-          "title": entry.title,
-          "thumbnail": NewsAggregator.get_thumbnail(entry),
-          "link": entry.link,
-          "summary": NewsAggregator.format_summary(entry.summary),
-          "date": NewsAggregator.datetime_from_string(
-            entry.published if "published" in entry.keys() else entry.date if "date" in entry.keys() else "recent")
-        } for entry in (parsed_feed.entries[0:10] if len(parsed_feed.entries) >= 10 else parsed_feed.entries)
-      ]
-    }
 
 
 # Print Entries
--- app/app.py
+++ app/app.py
@@ -1,21 +1,27 @@
 import flask
 from flask_moment import Moment
+import json
 
-import aggregator
+from aggregator import NewsAggregator
 
 
 app = flask.Flask("__name__")
-
 moment = Moment(app)
 
-entries = aggregator.NewsAggregator().get_latest()
-latest_feed = entries[:5]
-curated_feed = entries[5:9]
-more_feed = entries[10:15]
+with open("feeds.json", "r") as f:
+  _feeds = json.loads(f.read())
+  _latest_feeds = _feeds["latest"]
+  _top_feeds = _feeds["top"]
+  _curated_feeds = _feeds["curated"]
+
+agg = NewsAggregator()
+latest_feed = agg.get_latest(_latest_feeds, count=5)
+curated_feed = agg.get_latest(_curated_feeds, count=4)
+top_feed = agg.get_latest(_top_feeds, count=5)
 
 @app.route("/")
 def index():
-  return flask.render_template("index.html", latest_feed=latest_feed, curated_feed=curated_feed, more_feed=more_feed)
+  return flask.render_template("index.html", latest_feed=latest_feed, curated_feed=curated_feed, more_feed=top_feed)
 
 @app.route("/about")
 def about():
